0331144f017cf1547c9ea3cf43f681b3e6bf23da
Cole Aydelottte <cole.aydelotte@gmail.com>
2023-11-15 00:10:50 -0500

    Initial Testing
    Added Initial tests for the PurchasableItems class and added documentation to the PurchasableItems class.

--- src/com/shoppingcart/PurchasableItems.java
+++ src/com/shoppingcart/PurchasableItems.java
@@ -6,37 +6,61 @@ public class PurchasableItems
 {
     private ArrayList<Purchasable> Items;
 
-
+    /**
+     * Base case contructor; sets the private variable Items
+     * equal to a new ArrayList containing Purchasables.
+     */
     public PurchasableItems()
     {
         Items = new ArrayList<>();
     }
 
+    
+    
+    /**
+     * Constructor that takes a ArrayList containing Purchasables
+     * as a parameter and sets the private variable Items equal to
+     * the parameter list.
+     */
     public PurchasableItems(ArrayList<Purchasable> list)
     {
         Items = list;
     }
-    
+
+    /**
+     * Calls the constructor of Purchasable using the three
+     * parameters and a four digit number recieved from 
+     * getBarcode().
+     */
     public void createItem(int stock, double price, String name)
     {
-        int barcode = getUniqueBarcode();
+        int barcode = getBarcode();
         for (int x = 0; x < Items.size(); x++)
-        {
+        {         
             do
             {
-                barcode = getUniqueBarcode();
+                barcode = getBarcode();
             }
-            while(barcode != Items.get(x).getBarcode()); // Changes barcode in loop until doesnt equal an existing barcode
+            while(barcode == Items.get(x).getBarcode());
         }
         Purchasable item = new Purchasable(stock, barcode, price, name);
         Items.add(item);
     }
 
-    public int getUniqueBarcode()
+    /**
+     * Generates a unique four digit number and returns
+     * the Result.
+     */
+        public int getBarcode()
     {
         Random rand = new Random();
         int randNum = rand.nextInt(1000, 9999);
         return randNum;
+    }
+
+    public ArrayList<Purchasable> getItems()
+    {
+        return Items;
     }
 
     @Override
