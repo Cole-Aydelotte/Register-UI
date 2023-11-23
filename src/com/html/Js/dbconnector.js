2c2155f91857c8429c61efb0085bf4dcca03238a
Cole Aydelottte <150745978+Cole-Aydelotte@users.noreply.github.com>
2023-11-23 10:31:48 -0500

    Added the account.css File
    added the account.css File

---
+++ src/com/html/Js/dbconnector.js
@@ -0,0 +1,33 @@
+class dbconnector
+{
+    /**
+     * connects sql databse and will run dupfile.
+     */
+    connect()
+    {
+        const mysql = require('mysql');
+
+        const connection = mysql.createConnection({
+          host: 'localhost',
+          user: 'project',
+          password: 'project',
+          database: 'Register_UI'
+        });
+        
+        connection.connect((err) => {
+          if (err) {
+            console.error('Error connecting to database:', err);
+            return;
+          }
+          console.log('Connected to database');
+        });
+    }
+
+    insertInto(name, stock, price)
+    {
+        item = createItem(stock, price, name);
+        barcode = item.getBarcode();
+        connection.query('INSERT INTO PurchasableItems VALUES (name, stock, price, barcode);');
+    }
+
+}
